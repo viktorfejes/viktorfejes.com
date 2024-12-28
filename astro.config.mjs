2a60e13bca9122a87a95b22ce43164382c780e29
viktorfejes <viktor@viktorfejes.com>
2024-12-27 22:24:12 -0800

    Changed the style slightly once more. Nothing to see here... Still need to finish it up cleanly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://viktorfejes.com",
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx()]
 });
