8c34098ccdfd4a709b89dceda709862b84a42419
viktorfejes <viktor@viktorfejes.com>
2025-01-01 23:35:17 -0800

    Changed CSS to Tailwind for convenience and rapid building. Getting close to completing portfolio projects.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://viktorfejes.com",
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			maxWidth: {
+				'8xl': '120rem',
+			},
+			screens: {
+				'1024': '1024px'
+			}
+		},
 	},
 	plugins: [],
 }
