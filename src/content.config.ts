878fc1e48732489bb90778243da4cf197250bee3
viktorfejes <viktor@viktorfejes.com>
2025-07-01 23:50:43 -0700

    Added description to projects, small bug fixes, first draft of Chroma Scopes project article

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,17 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+    // Load Markdown and MDX files in the `src/content/blog/` directory.
+    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    // Type-check frontmatter using a schema
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: image().optional(),
+    }),
 });
 
 const projects = defineCollection({
@@ -21,6 +21,7 @@ const projects = defineCollection({
         thumbnail: z.string(),
         tags: z.array(z.string()),
         featured: z.boolean().optional(),
+        description: z.string().optional(),
     }),
 });
 
