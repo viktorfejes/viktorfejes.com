27c1f6b89bc31b4b97408f08e3782c30437001c8
viktorfejes <viktor@viktorfejes.com>
2025-07-06 21:06:31 -0700

    Made the vectors inside the web component more encapsulated

--- src/assets/scripts/vector-add.js
+++ src/assets/scripts/vector-add.js
@@ -65,34 +65,68 @@ class Point {
 }
 
 class Vector {
-    constructor({start, end, label = "", parent, key}) {
+    constructor({
+        start,
+        end,
+        label = {},
+        stroke = {},
+        parent,
+        key,
+        marker = ""
+    }) {
+
+        // Defaults for objects
+        this.label = {
+            text: "",
+            offset: 0.5,
+            ...label
+        };
+        this.stroke = {
+            width: 0.15,
+            color: "black",
+            ...stroke
+        };
+
         this.start = start;
         this.end = end;
-        this.label = label;
         this.parent = parent;
         this.key = key;
+        this.marker = marker;
 
         // Group
         this.group = createSVGElement("g");
 
         // Line
-        this.line = createSVGElement("line");
+        this.line = createSVGElement("line", {
+            "stroke-width": this.stroke.width,
+            "stroke": this.stroke.color
+        });
         this.line.classList.add("vector-line", `vector-line--${this.key}`);
+
+        // Marker
+        if (marker) {
+            this.marker.setAttribute("fill", this.stroke.color);
+            this.line.setAttribute("marker-end", `url(#${this.marker.id})`);
+        }
+
+        // Append line to the group
+        this.group.appendChild(this.line);
 
         // Label
         if (label) {
             this.labelEl = createSVGElement("text", {
+                fill: this.stroke.color,
                 "text-anchor": "middle",
                 "dominant-baseline": "middle",
-                transform: "scale(1,-1)"
+                transform: "scale(1 -1)"
             });
-            this.labelEl.textContent = label;
+            this.labelEl.textContent = label.text;
             this.labelEl.classList.add("vector-label", `vector-label--${this.key}`);
+
+            this.group.appendChild(this.labelEl);
         }
 
-        // Append everything appropriately.
-        this.group.appendChild(this.line);
-        if (label) this.group.appendChild(this.labelEl);
+        // Append to parent
         this.parent.appendChild(this.group);
 
         // Add listeners to points so they can call this update
@@ -104,16 +138,25 @@ class Vector {
 
     update() {
         const {x: sx, y: sy} = this.start;
-        const {x: ex, y: ey} = this.end;
+        let {x: ex, y: ey} = this.end;
 
-        const {x: lineX, y: lineY} = calculateVectorLineLength(this.start, this.end, 4, 3.1, 0.15);
+        if (this.marker) {
+            const {x: lineX, y: lineY} = calculateVectorLineLength(
+                this.start, this.end,
+                parseFloat(this.marker.getAttribute("markerWidth")),
+                parseFloat(this.marker.getAttribute("refX")),
+                this.stroke.width
+            );
+            ex = lineX;
+            ey = lineY;
+        }
         this.line.setAttribute("x1", sx);
         this.line.setAttribute("y1", sy);
-        this.line.setAttribute("x2", lineX);
-        this.line.setAttribute("y2", lineY);
+        this.line.setAttribute("x2", ex);
+        this.line.setAttribute("y2", ey);
 
         if (this.labelEl) {
-            const {x: labelX, y: labelY} = calculateLabelPosition(sx, sy, ex, ey, 0.5);
+            const {x: labelX, y: labelY} = calculateLabelPosition(sx, sy, ex, ey, this.label.offset);
             this.labelEl.setAttribute("x", labelX);
             this.labelEl.setAttribute("y", -labelY);
         }
@@ -157,8 +200,11 @@ class Handle {
         })
 
         if (interactive) {
+            this.el.style.cursor = "grab";
+
             this.el.addEventListener("pointerdown", (e) => {
                 this.dragging = true;
+                this.el.style.cursor = "grabbing";
 
                 // Moving
                 this.parent.addEventListener("pointermove", this.onPointerMove);
@@ -166,6 +212,7 @@ class Handle {
                 // End of moving
                 document.addEventListener("pointerup", () => {
                     this.dragging = false;
+                    this.el.style.cursor = "grab";
                     document.removeEventListener("pointermove", this.onPointerMove);
                 }, { once: true });
             });
@@ -228,7 +275,7 @@ class VectorAdd extends HTMLElement {
             vectorAux: isDark ? "#262626": "#e5e5e5",
         };
 
-        const vectorStrokeWidth = 0.1;
+        const vectorStrokeWidth = 0.15;
 
         // Empty the shadowDom
         shadow.innerHTML = "";
@@ -254,41 +301,17 @@ class VectorAdd extends HTMLElement {
                 stroke: ${colors.axis};
                 stroke-width: 0.05;
             }
-            .vector-line { stroke-width: ${vectorStrokeWidth}; }
-            .vector-line--vectorA { 
-                marker-end: url(#arrowhead-a);
-                stroke: ${colors.vectorA};
-            }
-            .vector-line--vectorB {
-                marker-end: url(#arrowhead-b);
-                stroke: ${colors.vectorB};
-            }
-            .vector-line--vectorSum {
-                marker-end: url(#arrowhead-sum);
-                stroke: ${colors.vectorSum};
-            }
             .vector-line--vectorGhostAB,
             .vector-line--vectorGhostBA {
-                marker-end: url(#arrowhead-aux);
-                stroke: ${colors.vectorAux};
                 stroke-dasharray: 0.3 0.1;
             }
             .vector-label {
                 user-select: none;
                 pointer-events: none;
-                font-family: "Geist Mono", sans-serif;
+                font-family: "Geist Mono", monospace;
                 font-size: 0.04rem;
                 font-weight: 500;
             }
-            .vector-label--vectorA { fill: ${colors.vectorA}; }
-            .vector-label--vectorB { fill: ${colors.vectorB}; }
-            .vector-label--vectorSum { fill: ${colors.vectorSum}; }
-            .vector-label--vectorGhostAB,
-            .vector-label--vectorGhostBA { fill: ${colors.vectorAux}; }
-            #arrowhead-a { fill: ${colors.vectorA}; }
-            #arrowhead-b { fill: ${colors.vectorB}; }
-            #arrowhead-sum { fill: ${colors.vectorSum}; }
-            #arrowhead-aux { fill: ${colors.vectorAux}; }
             .info-panel {
                 width: 100%;
                 display: flex;
@@ -300,9 +323,11 @@ class VectorAdd extends HTMLElement {
             }
             .vector-info {
                 background: ${colors.grid};
-                padding: 0.4rem 0.6rem;
+                padding: 0.4rem 0;
                 border-radius: 0.3rem;
                 white-space: nowrap;
+                width: 8rem;
+                text-align: center;
             }
             .vector-info.a {
                 color: ${colors.vectorA};
@@ -327,154 +352,189 @@ class VectorAdd extends HTMLElement {
         // Create SVG canvas
         const svg = createSVGElement("svg", {
             "viewBox": `${this.bounds.x} ${this.bounds.y} ${this.bounds.width} ${this.bounds.height}`,
-            transform: "scale(1,-1)"
+            transform: "scale(1 -1)"
         });
 
         // Create definitions for the SVG
-        {
-            const defs = createSVGElement("defs");
+        const defs = createSVGElement("defs");
 
-            // Create arrow(s)
-            const marker = createSVGElement("marker", {
-                "id": "arrowhead-a",
-                "markerWidth": 4,
-                "markerHeight": 4,
-                "orient": "auto",
-                "refX": 3.1,
-                "refY": 2
+        // Create arrow(s)
+        const arrowA = createSVGElement("marker", {
+            "id": "arrowhead-a",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+
+        const markerPoly = createSVGElement("polygon", {
+            "points": "0 0, 4 2, 0 4"
+        });
+        arrowA.appendChild(markerPoly);
+        defs.appendChild(arrowA);
+
+        const arrowB = createSVGElement("marker", {
+            "id": "arrowhead-b",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+        arrowB.appendChild(markerPoly.cloneNode(true));
+        defs.appendChild(arrowB);
+
+        const arrowSum = createSVGElement("marker", {
+            "id": "arrowhead-sum",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+        arrowSum.appendChild(markerPoly.cloneNode(true));
+        defs.appendChild(arrowSum);
+
+        const arrowAux = createSVGElement("marker", {
+            "id": "arrowhead-aux",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+        arrowAux.appendChild(markerPoly.cloneNode(true));
+        defs.appendChild(arrowAux);
+
+        svg.appendChild(defs);
+
+        // Background
+        // Horizontal grid lines
+        for (let i = this.bounds.y + 1; i < (this.bounds.y + this.bounds.height); ++i) {
+            const line = createSVGElement("line", {
+                x1: this.bounds.x,
+                y1: i,
+                x2: this.bounds.x + this.bounds.width,
+                y2: i
             });
-
-            const markerPoly = createSVGElement("polygon", {
-                "points": "0 0, 4 2, 0 4"
+            line.classList.add("grid-line");
+            svg.appendChild(line);
+        }
+        
+        // Vertical grid lines
+        for (let i = this.bounds.x + 1; i < (this.bounds.x + this.bounds.width); ++i) {
+            const line = createSVGElement("line", {
+                x1: i,
+                y1: this.bounds.y,
+                x2: i,
+                y2: this.bounds.y + this.bounds.height,
             });
-            marker.appendChild(markerPoly);
-            defs.appendChild(marker);
-
-            const arrowB = createSVGElement("marker", {
-                "id": "arrowhead-b",
-                "markerWidth": 4,
-                "markerHeight": 4,
-                "orient": "auto",
-                "refX": 3.1,
-                "refY": 2
-            });
-            arrowB.appendChild(markerPoly.cloneNode(true));
-            defs.appendChild(arrowB);
-
-            const arrowSum = createSVGElement("marker", {
-                "id": "arrowhead-sum",
-                "markerWidth": 4,
-                "markerHeight": 4,
-                "orient": "auto",
-                "refX": 3.1,
-                "refY": 2
-            });
-            arrowSum.appendChild(markerPoly.cloneNode(true));
-            defs.appendChild(arrowSum);
-
-            const arrowAux = createSVGElement("marker", {
-                "id": "arrowhead-aux",
-                "markerWidth": 4,
-                "markerHeight": 4,
-                "orient": "auto",
-                "refX": 3.1,
-                "refY": 2
-            });
-            arrowAux.appendChild(markerPoly.cloneNode(true));
-            defs.appendChild(arrowAux);
-
-            svg.appendChild(defs);
+            line.classList.add("grid-line");
+            svg.appendChild(line);
         }
 
-        // Create background and axes
-        {
-            // Background
-            // Horizontal grid lines
-            for (let i = this.bounds.y + 1; i < (this.bounds.y + this.bounds.height); ++i) {
-                const line = createSVGElement("line", {
-                    x1: this.bounds.x,
-                    y1: i,
-                    x2: this.bounds.x + this.bounds.width,
-                    y2: i
-                });
-                line.classList.add("grid-line");
-                svg.appendChild(line);
-            }
-            
-            // Vertical grid lines
-            for (let i = this.bounds.x + 1; i < (this.bounds.x + this.bounds.width); ++i) {
-                const line = createSVGElement("line", {
-                    x1: i,
-                    y1: this.bounds.y,
-                    x2: i,
-                    y2: this.bounds.y + this.bounds.height,
-                });
-                line.classList.add("grid-line");
-                svg.appendChild(line);
-            }
+        // X-axis
+        const axisX = createSVGElement("line", {
+            "x1": 0,
+            "y1": this.bounds.y,
+            "x2": 0,
+            "y2": this.bounds.height
+        });
+        // Y-axis
+        const axisY = createSVGElement("line", {
+            "x1": this.bounds.x,
+            "y1": 0,
+            "x2": this.bounds.width,
+            "y2": 0
+        });
 
-            // X-axis
-            const axisX = createSVGElement("line", {
-                "x1": 0,
-                "y1": this.bounds.y,
-                "x2": 0,
-                "y2": this.bounds.height
-            });
-            // Y-axis
-            const axisY = createSVGElement("line", {
-                "x1": this.bounds.x,
-                "y1": 0,
-                "x2": this.bounds.width,
-                "y2": 0
-            });
+        axisX.classList.add("axis-line");
+        axisY.classList.add("axis-line");
 
-            axisX.classList.add("axis-line");
-            axisY.classList.add("axis-line");
-
-            svg.appendChild(axisX);
-            svg.appendChild(axisY);
-        }
+        svg.appendChild(axisX);
+        svg.appendChild(axisY);
 
         // Create points for vectors (unconventionally: tail and head)
+        const p00 = new Point("p00", 0, 0);
+
         const vectorA = new Vector({
             start: new Point("va_s", 0, 0),
             end: new Point("va_e", 9, 3),
-            label: "A",
+            label: {
+                text: "A",
+            },
             parent: svg,
-            key: "vectorA"});
+            key: "vectorA",
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorA
+            },
+            marker: arrowA
+        });
 
         const vectorB = new Vector({
             start: new Point("vb_s", 0, 0),
             end: new Point("vb_e", -4, 4),
-            label: "B",
+            label: {
+                text: "B"
+            },
             parent: svg,
-            key: "vectorB"});
+            key: "vectorB",
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorB
+            },
+            marker: arrowB
+        });
 
-        const vectorSum = new Vector({
-            start: new Point("vsum_s", 0, 0),
-            end: new Point("vsum_e", 5, 7),
-            label: "A+B",
-            parent: svg,
-            key: "vectorSum"});
+        const pSum = new Point("psum", 5, 7);
 
         const vectorGhostAB = new Vector({
             start: vectorA.getEndPoint(),
-            end: vectorSum.getEndPoint(),
+            end: pSum,
             parent: svg,
             key: "vectorGhostAB",
-            label: "B"});
+            label: {
+                text: "B"
+            },
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorAux
+            },
+            marker: arrowAux
+        });
+
         const vectorGhostBA = new Vector({
             start: vectorB.getEndPoint(),
-            end: vectorSum.getEndPoint(),
+            end: pSum,
             parent: svg,
             key: "vectorGhostBA",
-            label: "A"});
+            label: {
+                text: "A"
+            },
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorAux
+            },
+            marker: arrowAux
+        });
 
-        function addVectors(target, a, b) {
-            return function update() {
-                target.set(a.x + b.x, a.y + b.y);
-            };
-        }
+        const vectorSum = new Vector({
+            start: p00,
+            end: pSum,
+            label: {
+                text: "A+B",
+                offset: 0.8
+            },
+            parent: svg,
+            key: "vectorSum",
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorSum
+            },
+            marker: arrowSum
+        });
 
         // Add handles
         const handleA = new Handle({
@@ -505,21 +565,19 @@ class VectorAdd extends HTMLElement {
         div.appendChild(info);
 
         vectorA.getEndPoint().onChange(() => {
-            const vsum = vectorSum.getEndPoint();
             const va = vectorA.getEndPoint();
             const vb = vectorB.getEndPoint();
-            vsum.set(va.x + vb.x, va.y + vb.y);
+            pSum.set(va.x + vb.x, va.y + vb.y);
             info.querySelector("#info-a").textContent = `(${va.x.toFixed(1)}, ${va.y.toFixed(1)})`;
-            info.querySelector("#info-sum").textContent = `(${vsum.x.toFixed(1)}, ${vsum.y.toFixed(1)})`;
+            info.querySelector("#info-sum").textContent = `(${pSum.x.toFixed(1)}, ${pSum.y.toFixed(1)})`;
         });
 
         vectorB.getEndPoint().onChange(() => {
-            const vsum = vectorSum.getEndPoint();
             const va = vectorA.getEndPoint();
             const vb = vectorB.getEndPoint();
-            vsum.set(va.x + vb.x, va.y + vb.y);
+            pSum.set(va.x + vb.x, va.y + vb.y);
             info.querySelector("#info-b").textContent = `(${vb.x.toFixed(1)}, ${vb.y.toFixed(1)})`;
-            info.querySelector("#info-sum").textContent = `(${vsum.x.toFixed(1)}, ${vsum.y.toFixed(1)})`;
+            info.querySelector("#info-sum").textContent = `(${pSum.x.toFixed(1)}, ${pSum.y.toFixed(1)})`;
         });
     }
 }
