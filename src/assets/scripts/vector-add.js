6e9b5da6574e4024fcdb0883f5af29ce4fd94443
viktorfejes <viktor@viktorfejes.com>
2025-07-07 16:23:06 -0700

    Added vector-magnitude to vector op visualizations

--- src/assets/scripts/vector-add.js
+++ src/assets/scripts/vector-add.js
@@ -190,6 +190,7 @@ class Handle {
             r: this.radius,
             fill: "transparent"
         });
+        this.el.classList.add("handle");
 
         this.parent.appendChild(this.el);
 
@@ -1191,7 +1192,7 @@ class VectorScale extends HTMLElement {
             const va = vectorA.getEndPoint();
             const scale = parseFloat(e.target.value);
             const scaled = { x: va.x * scale, y: va.y * scale };
-            console.log(scaled);
+
             vectorScaled.getEndPoint().set(scaled.x, scaled.y);
             sliderValue.textContent = `${scale.toFixed(1)}x`;
             
@@ -1211,8 +1212,281 @@ class VectorScale extends HTMLElement {
     }
 }
 
+class VectorMag extends HTMLElement {
+    constructor() {
+        super();
+
+        this.bounds = {
+            x: -10,
+            y: -8,
+            width: 20,
+            height: 16
+        };
+
+        // Create shadow DOM
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback() {
+        this.render();
+
+        document.addEventListener("theme-changed", () => {
+            this.render();
+        })
+    }
+
+    render() {
+        const shadow = this.shadowRoot;
+        const isDark = document.querySelector("html").classList.contains("dark");
+
+        const colors = {
+            background: isDark ? "#171717" : "#fafafa",
+            grid: isDark ? "#262626" : "#f5f5f5",
+            axis: isDark ? "#525252" : "#0a0a0a",
+            vectorA: isDark ? "#f43f5e" : "#f43f5e",
+            vectorB: isDark ? "#65a30d" : "#65a30d",
+        };
+
+        const vectorStrokeWidth = 0.15;
+
+        // Empty the shadowDom
+        shadow.innerHTML = "";
+
+        // Style
+        const style = document.createElement("style");
+        style.textContent = `
+            :host {
+                display: block;
+            }
+            .wrapper {
+                background: ${colors.background};
+                padding: 2rem 2rem 0 2rem;
+            }
+            .grid-line {
+                stroke: ${colors.grid};
+                stroke-width: 0.05;
+            }
+            .axis-line {
+                stroke: ${colors.axis};
+                stroke-width: 0.05;
+            }
+            .vector-label {
+                user-select: none;
+                pointer-events: none;
+                font-family: "Geist Mono", monospace;
+                font-size: 0.04rem;
+                font-weight: 500;
+            }
+            .handle {
+                touch-action: none;
+            }
+            .info-panel {
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-wrap: wrap;
+                gap: 0.75rem;
+                padding: 1rem 0;
+            }
+            .vector-info {
+                background: ${colors.grid};
+                padding: 0.4rem 0;
+                border-radius: 0.3rem;
+                white-space: nowrap;
+                width: 10rem;
+                text-align: center;
+            }
+            .vector-info.a {
+                color: ${colors.vectorA};
+                background: #fecdd3;
+            }
+            .vector-info.scaled {
+                color: ${colors.vectorB};
+                background: #ecfccb;
+            }
+        `;
+        shadow.appendChild(style);
+
+        // Create wrapping div
+        const div = document.createElement("div");
+        div.classList.add("wrapper");
+        shadow.appendChild(div);
+
+        // Create SVG canvas
+        const svg = createSVGElement("svg", {
+            "viewBox": `${this.bounds.x} ${this.bounds.y} ${this.bounds.width} ${this.bounds.height}`,
+        });
+
+        // Create definitions for the SVG
+        const defs = createSVGElement("defs");
+
+        // Create arrow(s)
+        const arrowA = createSVGElement("marker", {
+            "id": "arrowhead-a",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+
+        const markerPoly = createSVGElement("polygon", {
+            "points": "0 0, 4 2, 0 4"
+        });
+        arrowA.appendChild(markerPoly);
+        defs.appendChild(arrowA);
+
+        const arrowB = createSVGElement("marker", {
+            "id": "arrowhead-b",
+            "markerWidth": 4,
+            "markerHeight": 4,
+            "orient": "auto",
+            "refX": 3.1,
+            "refY": 2
+        });
+        arrowB.appendChild(markerPoly.cloneNode(true));
+        defs.appendChild(arrowB);
+
+        svg.appendChild(defs);
+
+        // Background
+        // Horizontal grid lines
+        for (let i = this.bounds.y + 1; i < (this.bounds.y + this.bounds.height); ++i) {
+            const line = createSVGElement("line", {
+                x1: this.bounds.x,
+                y1: i,
+                x2: this.bounds.x + this.bounds.width,
+                y2: i
+            });
+            line.classList.add("grid-line");
+            svg.appendChild(line);
+        }
+        
+        // Vertical grid lines
+        for (let i = this.bounds.x + 1; i < (this.bounds.x + this.bounds.width); ++i) {
+            const line = createSVGElement("line", {
+                x1: i,
+                y1: this.bounds.y,
+                x2: i,
+                y2: this.bounds.y + this.bounds.height,
+            });
+            line.classList.add("grid-line");
+            svg.appendChild(line);
+        }
+
+        // X-axis
+        const axisX = createSVGElement("line", {
+            "x1": 0,
+            "y1": this.bounds.y,
+            "x2": 0,
+            "y2": this.bounds.y + this.bounds.height
+        });
+        // Y-axis
+        const axisY = createSVGElement("line", {
+            "x1": this.bounds.x,
+            "y1": 0,
+            "x2": this.bounds.x + this.bounds.width,
+            "y2": 0
+        });
+
+        axisX.classList.add("axis-line");
+        axisY.classList.add("axis-line");
+
+        svg.appendChild(axisX);
+        svg.appendChild(axisY);
+
+        const g_flip = createSVGElement("g", {
+            transform: "scale(1 -1)"
+        });
+        svg.appendChild(g_flip);
+
+        // Create points for vectors (unconventionally: tail and head)
+        const p00 = new Point("p00", 0, 0);
+        const v1 = new Point("v1", 5, 5);
+        const ra = new Point("ra", 5, 0);
+
+        new Vector({
+            start: v1,
+            end: ra,
+            label: {
+                text: "Ay",
+                offset: -0.6
+            },
+            parent: g_flip,
+            key: "opposite",
+            stroke: {
+                width: 0.1,
+                color: colors.vectorB
+            },
+        });
+
+        new Vector({
+            start: p00,
+            end: ra,
+            label: {
+                text: "Ax",
+                offset: 0.6
+            },
+            parent: g_flip,
+            key: "adjacent",
+            stroke: {
+                width: 0.1,
+                color: colors.vectorB
+            },
+        });
+
+        const vector = new Vector({
+            start: p00,
+            end: v1,
+            label: {
+                text: "A",
+                offset: -0.5
+            },
+            parent: g_flip,
+            key: "vector",
+            stroke: {
+                width: vectorStrokeWidth,
+                color: colors.vectorA
+            },
+            marker: arrowA
+        });
+
+        // Add handles
+        new Handle({
+            point: vector.getEndPoint(),
+            radius: 1.2,
+            interactive: true,
+            parent: g_flip
+        });
+
+        // Append svg to shadow DOM
+        div.appendChild(svg);
+
+        function vLen(point) {
+            return Math.sqrt(point.x * point.x + point.y * point.y);
+        }
+
+        // Create displays at the bottom
+        const info = document.createElement("div");
+        info.classList.add("info-panel");
+        info.innerHTML = `
+            <div class="vector-info a">A: <span id="info-a">(${vector.getStr()})</span></div>
+            <div class="vector-info length">Length: <span id="info-length">${vLen(v1).toFixed(1)}</span></div>
+        `;
+        div.appendChild(info);
+
+        v1.onChange(() => {
+            ra.set(v1.x, 0);
+            info.querySelector("#info-a").textContent = `(${v1.x.toFixed(1)}, ${v1.y.toFixed(1)})`;
+            info.querySelector("#info-length").textContent = vLen(v1).toFixed(1);
+        });
+    }
+}
+
 customElements.define("vector-add", VectorAdd);
 customElements.define("vector-sub", VectorSub);
 customElements.define("vector-scale", VectorScale);
+customElements.define("vector-mag", VectorMag);
 
-export { VectorAdd, VectorSub, VectorScale };
+export { VectorAdd, VectorSub, VectorScale, VectorMag };
--- src/components/BlogComponents.js
+++ src/components/BlogComponents.js
@@ -1,16 +1,8 @@
-// TODO: Since these are not global, let's move these locally to the right mdx
-import VectorAdd from "./visualizations/VectorAdd.astro";
-import VectorSub from "./visualizations/VectorSub.astro";
-import VectorScale from "./visualizations/VectorScale.astro";
-
 import ImageAsset from "./ImageAsset.astro";
 import MetaList from "./MetaList.astro";
 import MetaItem from "./MetaItem.astro";
 
 export const components = {
-    VectorScale,
-    VectorSub,
-    VectorAdd,
     MetaList,
     MetaItem,
     ImageAsset
