412c8af24bca32062ed6c32fcd30ff27a5ab0242
viktorfejes <viktor@viktorfejes.com>
2025-07-06 22:53:06 -0700

    Started tinkering with Vector Subtraction component for vector op vis blog post

--- src/components/BlogComponents.js
+++ src/components/BlogComponents.js
@@ -1,10 +1,12 @@
 import VectorAdd from "./visualizations/VectorAdd.astro";
+import VectorSub from "./visualizations/VectorSub.astro";
 
 import ImageAsset from "./ImageAsset.astro";
 import MetaList from "./MetaList.astro";
 import MetaItem from "./MetaItem.astro";
 
 export const components = {
+    VectorSub,
     VectorAdd,
     MetaList,
     MetaItem,
