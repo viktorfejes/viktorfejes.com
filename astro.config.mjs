316a4a1f8229d6f96d1667492b0e7b25d7e1a0ea
viktorfejes <viktor@viktorfejes.com>
2025-07-11 17:34:50 -0700

    Added Shiki code highlighting, read-time, and some extra styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,26 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import remarkMath from "remark-math";
 import rehypeKatex from 'rehype-katex';
+import { remarkReadingTime } from "./src/assets/scripts/remark-reading-time.mjs";
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://viktorfejes.com',
-    integrations: [mdx({
-            remarkPlugins: [remarkMath],
-            rehypePlugins: [rehypeKatex]
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
+    integrations: [
+        mdx({
+            remarkPlugins: [remarkMath, remarkReadingTime],
+            rehypePlugins: [rehypeKatex],
+            shikiConfig: {
+                themes: {
+                    light: "catppuccin-latte",
+                    dark: "material-theme-darker",
+                },
+            },
         }),
         sitemap(),
         icon()
